a1b4790074ea72f2dbe66cbe363291c4c83f74f9
hongweijie <1462519292@qq.com>
2026-01-06 18:55:42 +0800

    cc

--- test.py
+++ test.py
@@ -1,34 +1,33 @@
 from vllm import LLM, SamplingParams
-def test_demo():
-    
 
-    compilation_config = {
-        "splitting_ops": [
-            "vllm.unified_attention",
-            "vllm.unified_attention_with_output",
-            "vllm.unified_attention_with_output_kunlun",
-            "vllm.mamba_mixer2",
-            "vllm.mamba_mixer",
-            "vllm.short_conv",
-            "vllm.linear_attention",
-            "vllm.plamo2_mamba_mixer",
-            "vllm.gdn_attention",
-            "vllm.sparse_attn_indexer",
-        ]
-    }
-    llm = LLM(model="/home/model/Qwen3-0.6B", compilation_config=compilation_config)
 
-    sampling_params = SamplingParams(
-        temperature=0.7,
-        max_tokens=128,
-    )
+compilation_config = {
+    "splitting_ops": [
+        "vllm.unified_attention",
+        "vllm.unified_attention_with_output",
+        "vllm.unified_attention_with_output_kunlun",
+        "vllm.mamba_mixer2",
+        "vllm.mamba_mixer",
+        "vllm.short_conv",
+        "vllm.linear_attention",
+        "vllm.plamo2_mamba_mixer",
+        "vllm.gdn_attention",
+        "vllm.sparse_attn_indexer",
+    ]
+}
+llm = LLM(model="/home/model/Qwen3-0.6B", compilation_config=compilation_config)
 
-    outputs = llm.generate(
-        ["介绍一下 vLLM"],
-        sampling_params,
-    )
-    print(outputs[0].outputs[0].text)
-    assert len(outputs[0].outputs[0].text) > 0
+sampling_params = SamplingParams(
+    temperature=0.7,
+    max_tokens=128,
+)
+
+outputs = llm.generate(
+    ["介绍一下 vLLM"],
+    sampling_params,
+)
+print(outputs[0].outputs[0].text)
+assert len(outputs[0].outputs[0].text) > 0
 
 
     
