5459892605025b0359b89c93a6f5ece23fb57128
hongweijie <1462519292@qq.com>
2026-01-06 17:57:57 +0800

    c

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
 from vllm import LLM, SamplingParams
 def test_demo():
     
-    llm = LLM(model="/home/model/Qwen3-0.6B", compilation_config="{"splitting_ops": ["vllm.unified_attention", "vllm.unified_attention_with_output", "vllm.unified_attention_with_output_kunlun", "vllm.mamba_mixer2","vllm.mamba_mixer","vllm.short_conv","vllm.linear_attention", "vllm.plamo2_mamba_mixer", "vllm.gdn_attention","vllm.sparse_attn_indexer"]}")
+
+    compilation_config = {
+        "splitting_ops": [
+            "vllm.unified_attention",
+            "vllm.unified_attention_with_output",
+            "vllm.unified_attention_with_output_kunlun",
+            "vllm.mamba_mixer2",
+            "vllm.mamba_mixer",
+            "vllm.short_conv",
+            "vllm.linear_attention",
+            "vllm.plamo2_mamba_mixer",
+            "vllm.gdn_attention",
+            "vllm.sparse_attn_indexer",
+        ]
+    }
+    llm = LLM(model="/home/model/Qwen3-0.6B", compilation_config=compilation_config)
 
     sampling_params = SamplingParams(
         temperature=0.7,
