d9fe37709006954655b9e1f36f0511b9d2e6dc56
hongweijie <1462519292@qq.com>
2026-01-06 17:14:28 +0800

    p800

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
-def test_demo():
 
-    assert 1 == 1
-    print("test is ok !!!")
+def test_demo():
+    from vllm import LLM, SamplingParams
+
+    llm = LLM(model="/home/models/Qwen3-1.7B")
+
+    sampling_params = SamplingParams(
+        temperature=0.7,
+        max_tokens=128,
+    )
+
+    outputs = llm.generate(
+        ["介绍一下 vLLM"],
+        sampling_params,
+    )
+    print(outputs[0].outputs[0].text)
+    assert len(outputs[0].outputs[0].text) > 0
+
+
+    
