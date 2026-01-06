672b544157924a84a90387bee67f949a411dbe33
hongweijie <1462519292@qq.com>
2026-01-06 17:47:06 +0800

    cccc

--- test.py
+++ test.py
@@ -2,16 +2,16 @@
 def test_demo():
     from vllm import LLM, SamplingParams
 
-    from pathlib import Path
+    import os
 
-    dir_path = Path("./Qwen3-0.6B")
+    dir_path = "./Qwen3-0.6B"
 
-    if dir_path.is_dir():
-        print(dir_path.resolve())
+    if os.path.isdir(dir_path):
+        print(os.path.abspath(dir_path))
     else:
         print("目录不存在")
 
-    llm = LLM(model="./Qwen3-0.6B", compilation_config="{"splitting_ops": ["vllm.unified_attention", "vllm.unified_attention_with_output", "vllm.unified_attention_with_output_kunlun", "vllm.mamba_mixer2","vllm.mamba_mixer","vllm.short_conv","vllm.linear_attention", "vllm.plamo2_mamba_mixer", "vllm.gdn_attention","vllm.sparse_attn_indexer"]}")
+    llm = LLM(model="/home/model/Qwen3-0.6B", compilation_config="{"splitting_ops": ["vllm.unified_attention", "vllm.unified_attention_with_output", "vllm.unified_attention_with_output_kunlun", "vllm.mamba_mixer2","vllm.mamba_mixer","vllm.short_conv","vllm.linear_attention", "vllm.plamo2_mamba_mixer", "vllm.gdn_attention","vllm.sparse_attn_indexer"]}")
 
     sampling_params = SamplingParams(
         temperature=0.7,
