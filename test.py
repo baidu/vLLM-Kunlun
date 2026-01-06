9d666ce898f4fceca394aab936f9e9d1d4663603
hongweijie <1462519292@qq.com>
2026-01-06 17:48:44 +0800

    ccc

--- test.py
+++ test.py
@@ -2,15 +2,6 @@
 def test_demo():
     from vllm import LLM, SamplingParams
 
-    import os
-
-    dir_path = "./Qwen3-0.6B"
-
-    if os.path.isdir(dir_path):
-        print(os.path.abspath(dir_path))
-    else:
-        print("目录不存在")
-
     llm = LLM(model="/home/model/Qwen3-0.6B", compilation_config="{"splitting_ops": ["vllm.unified_attention", "vllm.unified_attention_with_output", "vllm.unified_attention_with_output_kunlun", "vllm.mamba_mixer2","vllm.mamba_mixer","vllm.short_conv","vllm.linear_attention", "vllm.plamo2_mamba_mixer", "vllm.gdn_attention","vllm.sparse_attn_indexer"]}")
 
     sampling_params = SamplingParams(
