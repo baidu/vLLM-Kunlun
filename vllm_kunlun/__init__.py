4b34a3b71237f215700c4c753a6a374d0351c807
Marshall Ge <1004083966@qq.com>
2026-08-06 17:31:57 +0800

    code style
    Signed-off-by: Marshall Ge <1004083966@qq.com>

--- vllm_kunlun/__init__.py
+++ vllm_kunlun/__init__.py
@@ -273,6 +273,7 @@ _register_post_import_hook(
     _w8a8_int8_moe_apply,
 )
 
+
 # --- hook 6: select_int8_moe_backend in w8a8_int8 module ---------------
 # CompressedTensorsW8A8Int8MoEMethod.__init__ calls select_int8_moe_backend()
 # which only supports Triton/CUDA backends. On Kunlun XPU is_cuda()==False so
